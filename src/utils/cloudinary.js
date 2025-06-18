0ab1b7fce26fdd78fb9494b4ef5a61ba5b908c4a
vedant <vedanthande2244@gmail.com>
2025-06-18 23:36:38 +0530

    adding more cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,6 @@
-import { v2 as cloudinary } from "cloudinary";
+import {
+    v2 as cloudinary
+} from "cloudinary";
 import fs from "fs"
 
 cloudinary.config({
@@ -6,3 +8,19 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("file is being uploaded", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+cloudinary.v2.uploader.upload()
