cfdb9306b04640b0e718fc19ec6ce8eca5c367b8
vedant <vedanthande2244@gmail.com>
2025-06-19 21:26:27 +0530

    adding routes controllers

--- src/app.js
+++ src/app.js
@@ -20,6 +20,13 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import 
+
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter )
+
 export {
     app
 }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,4 @@
-const aasyncHandler = (requestHandler) => {
+const asyncHandler = (requestHandler) => {
     (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
